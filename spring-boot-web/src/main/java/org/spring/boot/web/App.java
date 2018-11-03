edcdaaa79a3ec8cc4dff48a33fae55ed6129a607
yangwenbang <1471275556@qq.com>
2018-11-03 14:14:46 +0800

    新增spring cloud

--- spring-boot-web/src/main/java/org/spring/boot/web/App.java
+++ spring-boot-web/src/main/java/org/spring/boot/web/App.java
@@ -1,5 +1,6 @@
 package org.spring.boot.web;
 
+import org.spring.boot.api.utils.MD5Util;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,10 @@ public class App
     
     @RequestMapping("/helloboot")
 	String index(){
-    	return "Hello Spring Boot ----!!!";
+    	String encode = MD5Util.MD5("ywb");
+    	
+    	return encode;
+//    	return "hello word";
 	}
+   
 }
